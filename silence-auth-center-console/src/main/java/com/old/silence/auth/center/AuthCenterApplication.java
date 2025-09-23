d14cf575d59d097fc424d29ca8e628ea3ac2c492
张孟伟 <1436373010@qq.com>
2025-09-23 20:17:06 +0800

    initiate

--- silence-auth-center-console/src/main/java/com/old/silence/auth/center/AuthCenterApplication.java
+++ silence-auth-center-console/src/main/java/com/old/silence/auth/center/AuthCenterApplication.java
@@ -2,12 +2,14 @@ package com.old.silence.auth.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author moryzang
  */
 @SpringBootApplication
-class AuthCenterApplication {
+@EnableDiscoveryClient
+public class AuthCenterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthCenterApplication.class, args);
